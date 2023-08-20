52641967394e36d79a38f9d93d7058306a25511b
Mihai Voina <mihaitza1058@yahoo.com>
2023-08-20 17:15:27 +0300

    projects

--- src/components/Body/Projects/Projects.js
+++ src/components/Body/Projects/Projects.js
@@ -16,104 +16,33 @@ const Projects = () => {
       >
         {(methods) => (
           <div className="all-cont">
-            <div data-aos="flip-left" data-aos-delay="100">
-              <div className="card-cont">
-                <div className="card">
-                  <img src={image3} alt="" className="card-img" />
-                  <div className="intro">
-                    <div className="h1-btn-cont">
-                      <h1 className="cardh1">hello</h1>
-                      <div className="btn-cont">
-                        <a className="cbtn1">
-                          <FaEye />
-                        </a>
-                        <a className="cbtn2">
-                          <FaGithub />
-                        </a>
+            {data.map((card, index) => (
+              <div
+                key={index}
+                data-aos="flip-left"
+                data-aos-delay={100 * (index + 1)}
+              >
+                <div className="card-cont">
+                  <div className="card">
+                    <img src={card.image} alt="" className="card-img" />
+                    <div className="intro">
+                      <div className="h1-btn-cont">
+                        <h1 className="cardh1">{card.title}</h1>
+                        <div className="btn-cont">
+                          <a className="cbtn1" href={card.urlFirst}>
+                            <FaEye />
+                          </a>
+                          <a className="cbtn2" href={card.urlSecond}>
+                            <FaGithub />
+                          </a>
+                        </div>
                       </div>
+                      <p className="cardp">{card.description}</p>
                     </div>
-
-                    <p className="cardp">
-                      perspiciatis deleniti accusamus dignissimos commodi omnis!
-                      Excepturi animi reiciendis eum quisquam corporis nostrum?
-                    </p>
                   </div>
                 </div>
               </div>
-            </div>
-
-            <div data-aos="flip-left" data-aos-delay="200">
-              <div className="card-cont">
-                <div className="card">
-                  <img src={image3} alt="" className="card-img" />
-                  <div className="intro">
-                    <div className="h1-btn-cont">
-                      <h1 className="cardh1">hello</h1>
-                      <div className="btn-cont">
-                        <a className="cbtn1">
-                          <FaEye />
-                        </a>
-                        <a className="cbtn2">
-                          <FaGithub />
-                        </a>
-                      </div>
-                    </div>
-                    <p className="cardp">
-                      perspiciatis deleniti accusamus dignissimos commodi omnis!
-                      Excepturi animi reiciendis eum quisquam corporis nostrum?
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div data-aos="flip-left" data-aos-delay="300">
-              <div className="card-cont">
-                <div className="card">
-                  <img src={image3} alt="" className="card-img" />
-                  <div className="intro">
-                    <div className="h1-btn-cont">
-                      <h1 className="cardh1">hello</h1>
-                      <div className="btn-cont">
-                        <a className="cbtn1">
-                          <FaEye />
-                        </a>
-                        <a className="cbtn2">
-                          <FaGithub />
-                        </a>
-                      </div>
-                    </div>
-                    <p className="cardp">
-                      perspiciatis deleniti accusamus dignissimos commodi omnis!
-                      Excepturi animi reiciendis eum quisquam corporis nostrum?
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div data-aos="flip-left" data-aos-delay="400">
-              <div className="card-cont">
-                <div className="card">
-                  <img src={image3} alt="" className="card-img" />
-                  <div className="intro">
-                    <div className="h1-btn-cont">
-                      <h1 className="cardh1">hello</h1>
-                      <div className="btn-cont">
-                        <a className="cbtn1">
-                          <FaEye />
-                        </a>
-                        <a className="cbtn2">
-                          <FaGithub />
-                        </a>
-                      </div>
-                    </div>
-                    <p className="cardp">
-                      perspiciatis deleniti accusamus dignissimos commodi omnis!
-                      Excepturi animi reiciendis eum quisquam corporis nostrum?
-                    </p>
-                  </div>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         )}
       </WaterWave>
--- src/components/Body/Projects/ProjectsData.js
+++ src/components/Body/Projects/ProjectsData.js
@@ -1,34 +1,50 @@
 import { FaEye, FaGithub } from "react-icons/fa";
 import image from "./image.jpg";
+import shop from "./shop.png";
+import login from "./login.png";
+import cocktails from "./cocktails.png";
+import redux from "./ReduxShop.png";
 
 const data = [
   {
-    image: image,
-    title: "Card 1",
-    description: "Descriere pentru cardul 1",
+    image: shop,
+    title: "Context Shop",
+    description:
+      "  perspiciatis deleniti accusamus dignissimos commodi omnis! Excepturi animi reiciendis eum quisquam corporis nostrum?",
     firstIcon: <FaEye />,
     secondIcon: <FaGithub />,
+    urlFirst: "https://mihai-context-shop.netlify.app/",
+    urlSecond: "https://github.com/Mihai9088/licentaRez",
   },
   {
-    image: image,
-    title: "Card 2",
-    description: "Descriere pentru cardul 2",
+    image: redux,
+    title: "Redux Shop",
+    description:
+      "  perspiciatis deleniti accusamus dignissimos commodi omnis! Excepturi animi reiciendis eum quisquam corporis nostrum?",
     firstIcon: <FaEye />,
     secondIcon: <FaGithub />,
+    urlFirst: "https://mihai-redux-shop.netlify.app",
+    urlSecond: "https://github.com/Mihai9088/ReduxShop",
   },
   {
-    image: image,
-    title: "Card 3",
-    description: "Descriere pentru cardul 2",
+    image: login,
+    title: "Login Form",
+    description:
+      "  perspiciatis deleniti accusamus dignissimos commodi omnis! Excepturi animi reiciendis eum quisquam corporis nostrum?",
     firstIcon: <FaEye />,
     secondIcon: <FaGithub />,
+    urlFirst: " https://mihai-login-form.netlify.app",
+    urlSecond: "https://github.com/Mihai9088/Login-Form",
   },
   {
-    image: image,
-    title: "Card 4",
-    description: "Descriere pentru cardul 2",
+    image: cocktails,
+    title: "Cocktails App",
+    description:
+      "  perspiciatis deleniti accusamus dignissimos commodi omnis! Excepturi animi reiciendis eum quisquam corporis nostrum?",
     firstIcon: <FaEye />,
     secondIcon: <FaGithub />,
+    urlFirst: "https://mihai-cocktails.netlify.app",
+    urlSecond: "https://github.com/Mihai9088/Cocktails",
   },
 ];
 
