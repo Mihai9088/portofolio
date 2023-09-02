28e16950bdd7cf5f1ce6425bf28af7ab8c2f275b
Mihai Voina <mihaitza1058@yahoo.com>
2023-09-03 01:12:48 +0300

    form validation

--- src/components/Footer/Form.js
+++ src/components/Footer/Form.js
@@ -8,28 +8,61 @@ const Form = () => {
   const form = useRef();
   const [isSent, setIsSent] = useState(false);
   const [isFormSubmitted, setIsFormSubmitted] = useState(false);
+  const [errorMessages, setErrorMessages] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const validateForm = () => {
+    const { user_name, user_email, message } = form.current;
+    const errors = {};
+
+    if (user_name.value.length < 3) {
+      errors.name = "Name must have at least three letters.";
+    }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(user_email.value)) {
+      errors.email = "Email address is not valid.";
+    }
+
+    const [localPart, domain] = user_email.value.split("@");
+    if (!localPart || !domain) {
+      errors.email = "Email address is not valid.";
+    }
+
+    if (message.value.length < 10) {
+      errors.message = "The message must have at least ten letters.";
+    }
+
+    setErrorMessages(errors);
+    return Object.keys(errors).length === 0;
+  };
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_abmp1dn",
-        "template_ybg1vnk",
-        form.current,
-        "VYQ-rzHZsqXgcljZ-"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setIsSent(true);
-          setIsFormSubmitted(true);
-        },
-        (error) => {
-          console.log(error.text);
-          setIsSent(false);
-        }
-      );
+    if (validateForm()) {
+      emailjs
+        .sendForm(
+          "service_abmp1dn",
+          "template_ybg1vnk",
+          form.current,
+          "VYQ-rzHZsqXgcljZ-"
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+            setIsSent(true);
+            setIsFormSubmitted(true);
+          },
+          (error) => {
+            console.log(error.text);
+            setIsSent(false);
+          }
+        );
+    }
   };
 
   return (
@@ -39,14 +72,23 @@ const Form = () => {
         {isFormSubmitted ? (
           <p className="success-message">Form submitted successfully!</p>
         ) : (
-          <form ref={form} onSubmit={sendEmail}>
+          <form ref={form} onSubmit={sendEmail} noValidate>
             <input type="text" name="user_name" placeholder="ENTER YOUR NAME" />
+            {errorMessages.name && (
+              <p className="error-message">{errorMessages.name}</p>
+            )}
             <input
               type="email"
               name="user_email"
               placeholder="ENTER YOUR EMAIL"
             />
+            {errorMessages.email && (
+              <p className="error-message">{errorMessages.email}</p>
+            )}
             <textarea name="message" placeholder="WRITE YOUR MESSAGE" />
+            {errorMessages.message && (
+              <p className="error-message">{errorMessages.message}</p>
+            )}
             <input type="submit" value="Send" />
           </form>
         )}
